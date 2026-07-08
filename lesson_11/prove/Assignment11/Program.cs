f0c9e0da06043fd64b28ae5fc24c545914a8ddd7
vai-school <vai22005@byui.edu>
2026-07-07 19:22:35 -0600

    Assignment C# stuff

--- lesson_11/prove/Assignment11/Program.cs
+++ lesson_11/prove/Assignment11/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 
 namespace assignment11;
 
@@ -6,6 +7,9 @@ public class Assignment11
 {
     private const long START_NUMBER = 10_000_000_000;
     private const int RANGE_COUNT = 1_000_000;
+
+    private const int NUM_THREADS = 10;
+    private static int primeCount = 0;
 
     private static bool IsPrime(long n)
     {
@@ -24,21 +28,45 @@ public class Assignment11
     {
         // Use local variables for counting since we are in a single thread.
         int numbersProcessed = 0;
-        int primeCount = 0;
+
+
+        List<Thread> threads = [];
+        BlockingCollection<long> queue = [];
 
         Console.WriteLine("Prime numbers found:");
 
         var stopwatch = Stopwatch.StartNew();
+
+        for(int i = 0; i < NUM_THREADS; i++)
+        {
+            Thread thread = new(() =>
+            {
+               foreach (long number in queue.GetConsumingEnumerable())
+                {
+                    if (IsPrime(number))
+                    {
+                        Interlocked.Increment(ref primeCount);
+                        Console.Write($"{number}, ");
+                    }
+                } 
+            });
+
+            threads.Add(thread);
+            thread.Start();
+            
+        }
         
         // A single for-loop to check every number sequentially.
         for (long i = START_NUMBER; i < START_NUMBER + RANGE_COUNT; i++)
         {
+            queue.Add(i);
             numbersProcessed++;
-            if (IsPrime(i))
-            {
-                primeCount++;
-                Console.Write($"{i}, ");
-            }
+        }
+
+        queue.CompleteAdding();
+        foreach (Thread t in threads)
+        {
+            t.Join();
         }
 
         stopwatch.Stop();
